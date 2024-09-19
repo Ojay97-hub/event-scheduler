eb127cf3c274439f6899ff69868c7564b334961b
Owen Cotter <owenjames97@outlook.com>
2024-09-19 16:40:52 +0000

    debug == false for redeploy

--- event_scheduler/settings.py
+++ event_scheduler/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-ojay97hub-eventschedule-xsfo1a8oqzj.ws.codeinstitute-ide.net',

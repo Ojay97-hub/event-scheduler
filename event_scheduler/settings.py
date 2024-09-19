51b0ca1dfd6d0072562604717867960da6c89c9d
Owen Cotter <owenjames97@outlook.com>
2024-09-19 14:46:08 +0000

    ready for heroku deploy and createsuperuser

--- event_scheduler/settings.py
+++ event_scheduler/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xrni9)$bqod&!8=x52z+bj*zu0%we%(jmjof6pj**-x$hw9&=='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ojay97hub-eventschedule-xsfo1a8oqzj.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = [
+    '8000-ojay97hub-eventschedule-xsfo1a8oqzj.ws.codeinstitute-ide.net',
+    '.herokuapp.com',
+]
 
 
 # Application definition
@@ -74,13 +81,16 @@ WSGI_APPLICATION = 'event_scheduler.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

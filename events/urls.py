27bd8756dcb99bf7f70fbfc4dc0be5c4827d9f94
Owen Cotter <owenjames97@outlook.com>
2024-09-25 16:09:28 +0000

    added a bootstrap create event page view/template as a form

--- events/urls.py
+++ events/urls.py
@@ -1,8 +1,9 @@
 from . import views
 from django.urls import path
-from .views import HomeView, EventsList
+from .views import HomeView, EventsList, create_event
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),  # Homepage
+    path('create/', create_event, name='create_event'), # Create event
     path('events/', EventsList.as_view(), name='Event_Page'),  # Events page
 ]
--- events/views.py
+++ events/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from django.views.generic import TemplateView
 from .models import Event
+from .forms import EventForm
 
-# Create your views here.
+# Create your views here. 
 class EventsList(generic.ListView):
     queryset = Event.objects.all() # can add filter queryset to status 1 to show only published events 
     template_name = "events/event_list.html"
@@ -11,3 +12,14 @@ class EventsList(generic.ListView):
 
 class HomeView(TemplateView):
     template_name = 'home.html' 
+
+def create_event(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('Event_Page')
+    else:
+        form = EventForm()
+
+    return render(request, 'create_event.html', {'form': form})

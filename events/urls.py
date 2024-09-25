fa64873c87a14bb5831da6455bb63283247f9f69
Owen Cotter <owenjames97@outlook.com>
2024-09-25 15:17:08 +0000

    Added a basic home page template for future customisation

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
+from .views import HomeView, EventsList
 
 urlpatterns = [
-    path('', views.EventsList.as_view(), name='Event_Page'),
+    path('', HomeView.as_view(), name='home'),  # Homepage
+    path('events/', EventsList.as_view(), name='Event_Page'),  # Events page
 ]
--- events/views.py
+++ events/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic import TemplateView
 from .models import Event
 
 # Create your views here.
 class EventsList(generic.ListView):
     queryset = Event.objects.all() # can add filter queryset to status 1 to show only published events 
-    template_name = "events/index.html"
+    template_name = "events/event_list.html"
     paginate_by = 6
+
+class HomeView(TemplateView):
+    template_name = 'home.html' 

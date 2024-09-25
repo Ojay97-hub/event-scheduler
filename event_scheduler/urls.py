56b67447bc4a563bb86df08b8026342af32b2469
Owen Cotter <owenjames97@outlook.com>
2024-09-25 16:47:33 +0000

    installed & setup django allauth and customised login.html

--- event_scheduler/urls.py
+++ event_scheduler/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include("events.urls"), name='event-urls'),
